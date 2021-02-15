b2f402a1e971d38c005f6d7eb126afc634ade5cc
arsvarg <arsvarg8@gmail.com>
2021-02-15 21:30:15 +0700

    CameraMovement
    говно, но работает
    Камера следует за мышкой, но держит персонажа в кадре. Не выходит за края карты.

---
+++ Brainlet/Assets/MouseTracker.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseTracker : MonoBehaviour
+{
+    [SerializeField] Camera cam;
+    [SerializeField] Transform player;
+    [SerializeField] float threshold;
+
+    
+    void Update()
+    {
+        Vector3 mousePos = cam.ScreenToWorldPoint(Input.mousePosition);
+        Vector3 targetPos = (player.position + mousePos) / 2f;
+
+        targetPos.x = Mathf.Clamp(targetPos.x, -threshold + player.position.x, threshold + player.position.x);
+        targetPos.y = Mathf.Clamp(targetPos.y, -threshold + player.position.y, threshold + player.position.y);
+
+        this.transform.position = targetPos;
+    }
+}
--- Brainlet/Assets/Truba.cs
+++
@@ -1,8 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class Truba : MonoBehaviour
-{
-    public int weaponCode = 3;
-}
